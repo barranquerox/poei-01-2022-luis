40f61435e4cb7dc6211acbe824e26efd1e4da3ab
Luis Garelli <luiscarlosgarelli@gmail.com>
2022-01-04 15:48:44 +0100

    added test tp2 ajouter premier resultat

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -24,6 +24,32 @@ public class tp1 {
 
   @Test
   public void test2() {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.amazon.fr");
+    driver.manage().window().maximize();
+    // fermer cookies
+    WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
+    buttonCookies.click();
+    WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
+    barreRecherche.sendKeys("machine a raclette");
+    barreRecherche.sendKeys(Keys.ENTER);
 
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    WebElement premierResultat = driver.findElement(By.cssSelector("[data-cel-widget='search_result_1']"));
+    premierResultat.click();
+    WebElement ajouterPanierBouton = driver.findElement(By.cssSelector("#add-to-cart-button"));
+    ajouterPanierBouton.click();
+
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    driver.quit();
   }
 }
